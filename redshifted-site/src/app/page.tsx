36165d68692c916e9afca74782931d309d2bff22
Delta <deltarune5609@gmail.com>
2025-06-13 07:42:18 -0400

    Fixed TypeScript errors

--- redshifted-site/src/app/page.tsx
+++ redshifted-site/src/app/page.tsx
@@ -4,8 +4,8 @@ import { motion } from "framer-motion"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import Link from "next/link"
-import Image from "next/image";
-import banner from "@/app/banner.png";
+{/*import Image from "next/image";
+import banner from "@/app/banner.png";*/}
 
 export default function LandingPage() {
   return (
