a4ab54c5a12213f62e50192a5254465a0d356251
Delta <deltarune5609@gmail.com>
2025-07-13 17:03:42 -0400

    Site Revamp v2 - Deployment Fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint-config-next';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +10,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = [{
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  extends: ['next', 'next/core-web-vitals'],
+  rules: {
+    '@next/next/no-img-element': 'off', // Disable the warning for <img> usage
+  },
+}];
 
 export default eslintConfig;
